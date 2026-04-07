3021258408bcdf534159ea5db29e94f607bf14fe
Marysol102 <marysol102@gmx.es>
2026-04-07 16:20:38 +0200

    Delete js/config.js

--- js/config.js
+++
@@ -1,6 +0,0 @@
-// ── Constantes globales ──────────────────────────────────────
-const N       = 5;
-const SHAPES  = ['circle','square','triangle','x','diamond'];
-const COLORS  = ['red','blue','green','yellow','white'];
-const C_HEX   = {red:'#E05252',blue:'#4D6FD1',green:'#4EA84E',yellow:'#D4AE2C',white:'#E8E8E8'};
-const SPRIO   = {square:0, x:1, diamond:2, triangle:3, circle:4};
